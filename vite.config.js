bb9cff951e30b1360b3730df6e2e8d13e919bb23
Doan Hoang Thien Phu <dhtphu05@gmail.com>
2025-03-03 19:38:38 +0700

    rebase url

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/pokemon-page-vuejs/',
+  base: '/',
   plugins: [
     vue(),
     vueDevTools(),
